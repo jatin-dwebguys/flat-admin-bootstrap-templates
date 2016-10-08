9210385fffb1b6aa981641a95daa60423b0930a1
tui2tone <tui2tone@gmail.com>
2016-10-08 21:41:25 +0700

    split theme into singular file

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 ### Flat Admin V.3 [![Build Status](https://travis-ci.org/tui2tone/flat-admin-bootstrap-templates.svg?branch=dev-v3)](https://travis-ci.org/tui2tone/flat-admin-bootstrap-templates)
 #### Free Flat Design Administrator Bootstrap Templates
 
-- Static Html
-- AngularJS 1.x
-- Bootstrap 3.3.6
+Completely free bootstrap administrator templates with Bootstrap 3.
+
+- Responsive admin templates for all devices.
+- AngularJS and Static Html Version
+- A lot of UI Kits with Bootstrap
+  - Bootstrap Elements
+  - Pricing Table
+  - Chartist
+  - Card Design
+  - Steps
+  - Form
+  - Datatable
+  - Login
+- Build with ES6 and SASS
+- Easy Theming
 
 #### Showcase
 ![Show Case](http://templates.tui2tone.com/flat-admin/assets/images/flat-admin-showcase-mini.jpg)
-
-#### Structure
-
-#### Easy Intregrade with Many Framework
-
-- Ruby on Rails
-- ExpressJS
 
 #### Live Demo
 
 - [Static Html Version](http://templates.tui2tone.com/flat-admin/v3/html/index.html)
 - [AngularJS Version](http://templates.tui2tone.com/flat-admin/v3/angularjs/index.html)
+
+#### Easy Intregrade with Many Framework
+
+- Ruby on Rails
+- Laravel
 
 #### License
 
--- tasks/sass.js
+++ tasks/sass.js
@@ -10,7 +10,10 @@ import concat from 'gulp-concat';
 import autoprefixer from 'autoprefixer';
 import stripCssComments from 'gulp-strip-css-comments';
 
-gulp.task("sass", () => {
+
+gulp.task("sass", ["sass:main", "sass:theme"]);
+
+gulp.task("sass:main", () => {
   return gulp.src("./src/app.sass")
     .pipe(plumber({errorHandler: notify.onError(
       {
@@ -28,8 +31,27 @@ gulp.task("sass", () => {
     .pipe(gulp.dest('./temp/angularjs/assets/css/'))
 });
 
+gulp.task("sass:theme", () => {
+  return gulp.src(["./src/style/theme/*.sass", "!./src/style/theme/mixin.sass"])
+    .pipe(plumber({errorHandler: notify.onError(
+      {
+        title: "CSS Error: Line <%= error.line %>",
+        message: "<%= error.message %>"
+      })
+    }))
+    .pipe(sass())
+    .pipe(postcss([ 
+      autoprefixer({ browsers: ['last 2 versions'] })
+    ]))
+    .pipe(stripCssComments())
+    .pipe(gulp.dest('./temp/html/assets/css/theme'))
+    .pipe(gulp.dest('./temp/angularjs/assets/css/theme'))
+});
 
-gulp.task("build:sass", () => {
+
+gulp.task("build:sass", ["build:sass:main", "build:sass:theme"]);
+
+gulp.task("build:sass:main", () => {
   return gulp.src("./src/app.sass")
     .pipe(sass())
     .pipe(postcss([ 
@@ -39,4 +61,15 @@ gulp.task("build:sass", () => {
     .pipe(stripCssComments())
     .pipe(gulp.dest('./dist/html/assets/css/'))
     .pipe(gulp.dest('./dist/angularjs/assets/css/'))
+});
+
+gulp.task("build:sass:theme", () => {
+  return gulp.src(["./src/style/theme/*.sass", "!./src/style/theme/mixin.sass"])
+    .pipe(sass())
+    .pipe(postcss([ 
+      autoprefixer({ browsers: ['last 2 versions'] })
+    ]))
+    .pipe(stripCssComments())
+    .pipe(gulp.dest('./temp/html/assets/css/theme'))
+    .pipe(gulp.dest('./temp/angularjs/assets/css/theme'))
 });
