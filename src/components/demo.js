43ce88c3e097255617d3ebe0a37915b537d5ebde
tui2tone <tui2tone@gmail.com>
2016-10-20 01:32:11 +0700

    add dashboard real-time chart

--- src/components/demo.js
+++ src/components/demo.js
@@ -88,3 +88,182 @@ if($('.ct-chart-sale').length) {
     }]
   ])
 }
+
+
+if($('#flot-chart-dashboard').length) { 
+  var data = [],
+    data2 = [],
+    totalPoints = 50,
+    updateInterval = 1000;
+
+  function getRandomData() {
+
+    if (data.length > 0)
+      data = data.slice(1);
+
+    // Do a random walk
+
+    while (data.length < totalPoints) {
+
+      var prev = data.length > 0 ? data[data.length - 1] : 50,
+        y = prev + Math.random() * 10 - 5;
+
+      if (y < 0) {
+        y = 0;
+      } else if (y > 100) {
+        y = 100;
+      }
+
+      data.push(y);
+    }
+
+    // Zip the generated y values with the x values
+
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+      res.push([i, data[i]])
+    }
+
+    return res;
+  }
+
+  // Set up the control widget
+
+  var plot = $.plot("#flot-chart-dashboard", [ {
+    data: getRandomData(),
+    bars: {
+      show: true
+    }
+  }], {
+    series: {
+      shadowSize: 0, // Drawing is faster without shadows
+      bars: {
+        lineWidth: 1,
+        fill: true,
+        fillColor: "#39c3da"
+      }
+    },
+    yaxis: {
+      min: 0,
+      max: 100,
+      show: false
+    },
+    xaxis: {
+      show: false,
+      min: 0,
+      max: 50
+    },
+    colors: ["#FFF"],
+    grid: {
+      margin: 0,
+      axisMargin: 0,
+      borderWidth: 0
+    }
+  });
+
+  function update() {
+
+    plot.setData([{
+      data: getRandomData(),
+      bars: {
+        show: true
+      }
+    }]);
+
+    // Since the axes don't change, we don't need to call plot.setupGrid()
+
+    plot.draw();
+    setTimeout(update, updateInterval);
+  }
+
+  update();
+}
+
+
+if($('#flot-chart-monitoring').length) { 
+  var data2 = [],
+    totalPoints = 50,
+    updateInterval = 1000;
+
+  function getRandomData2() {
+
+    if (data2.length > 0)
+      data2 = data2.slice(1);
+
+    // Do a random walk
+
+    while (data2.length < totalPoints) {
+
+      var prev = data2.length > 0 ? data2[data2.length - 1] : 50,
+        y = prev + Math.random() * 10 - 5;
+
+      if (y < 0) {
+        y = 0;
+      } else if (y > 100) {
+        y = 100;
+      }
+
+      data2.push(y);
+    }
+
+    // Zip the generated y values with the x values
+
+    var res = [];
+    for (var i = 0; i < data2.length; ++i) {
+      res.push([i, data2[i]])
+    }
+
+    return res;
+  }
+
+  // Set up the control widget
+
+  var plot2 = $.plot("#flot-chart-monitoring", [ {
+    data: getRandomData2(),
+    bars: {
+      show: true
+    }
+  }], {
+    series: {
+      shadowSize: 0, // Drawing is faster without shadows
+      bars: {
+        lineWidth: 1,
+        fill: true,
+        fillColor: "#FFBC11"
+      }
+    },
+    yaxis: {
+      min: 0,
+      max: 100,
+      show: false
+    },
+    xaxis: {
+      show: false,
+      min: 0,
+      max: 50
+    },
+    colors: ["#FFF"],
+    grid: {
+      margin: 0,
+      axisMargin: 0,
+      borderWidth: 0
+    }
+  });
+
+  function updateMonitoring() {
+
+    plot2.setData([{
+      data: getRandomData2(),
+      bars: {
+        show: true
+      }
+    }]);
+
+    // Since the axes don't change, we don't need to call plot.setupGrid()
+
+    plot2.draw();
+    setTimeout(updateMonitoring, updateInterval);
+  }
+
+  updateMonitoring();
+}
--- src/elements/chart/flot.js
+++ src/elements/chart/flot.js
@@ -1,102 +1,104 @@
 
-var data = [],
-  data2 = [],
-  totalPoints = 300,
-  updateInterval = 250;
+if($('#flot-chart').length) { 
+  var data = [],
+    data2 = [],
+    totalPoints = 300,
+    updateInterval = 250;
 
-function getRandomData() {
+  function getRandomData() {
 
-  if (data.length > 0)
-    data = data.slice(1);
+    if (data.length > 0)
+      data = data.slice(1);
 
-  // Do a random walk
+    // Do a random walk
 
-  while (data.length < totalPoints) {
+    while (data.length < totalPoints) {
 
-    var prev = data.length > 0 ? data[data.length - 1] : 50,
-      y = prev + Math.random() * 10 - 5;
+      var prev = data.length > 0 ? data[data.length - 1] : 50,
+        y = prev + Math.random() * 10 - 5;
 
-    if (y < 0) {
-      y = 0;
-    } else if (y > 100) {
-      y = 100;
+      if (y < 0) {
+        y = 0;
+      } else if (y > 100) {
+        y = 100;
+      }
+
+      data.push(y);
     }
 
-    data.push(y);
-  }
+    // Zip the generated y values with the x values
 
-  // Zip the generated y values with the x values
-
-  var res = [];
-  for (var i = 0; i < data.length; ++i) {
-    res.push([i, data[i]])
-  }
-
-  return res;
-}
-
-
-function getRandomData2() {
-
-  if (data2.length > 0)
-    data2 = data2.slice(1);
-
-  // Do a random walk
-
-  while (data2.length < totalPoints) {
-
-    var prev = data2.length > 0 ? data2[data2.length - 1] : 50,
-      y = prev + Math.random() * 10 - 5;
-
-    if (y < 0) {
-      y = 0;
-    } else if (y > 100) {
-      y = 100;
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+      res.push([i, data[i]])
     }
 
-    data2.push(y);
+    return res;
   }
 
-  // Zip the generated y values with the x values
 
-  var res = [];
-  for (var i = 0; i < data.length; ++i) {
-    res.push([i, data2[i]])
+  function getRandomData2() {
+
+    if (data2.length > 0)
+      data2 = data2.slice(1);
+
+    // Do a random walk
+
+    while (data2.length < totalPoints) {
+
+      var prev = data2.length > 0 ? data2[data2.length - 1] : 50,
+        y = prev + Math.random() * 10 - 5;
+
+      if (y < 0) {
+        y = 0;
+      } else if (y > 100) {
+        y = 100;
+      }
+
+      data2.push(y);
+    }
+
+    // Zip the generated y values with the x values
+
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+      res.push([i, data2[i]])
+    }
+
+    return res;
   }
 
-  return res;
+  // Set up the control widget
+
+  var plot = $.plot("#flot-chart", [ getRandomData(), getRandomData2()], {
+    series: {
+      shadowSize: 0 // Drawing is faster without shadows
+    },
+    yaxis: {
+      min: 0,
+      max: 100,
+      color: "#BBB",
+      tickColor: "#EEE"
+    },
+    xaxis: {
+      show: false
+    },
+    colors: ["#29c75f", "#39c3da"],
+    grid: {
+      margin: 0,
+      borderWidth: 0
+    }
+  });
+
+  function update() {
+
+    plot.setData([getRandomData(), getRandomData2()]);
+
+    // Since the axes don't change, we don't need to call plot.setupGrid()
+
+    plot.draw();
+    setTimeout(update, updateInterval);
+  }
+
+  update();
 }
-
-// Set up the control widget
-
-var plot = $.plot("#flot-chart", [ getRandomData(), getRandomData2()], {
-  series: {
-    shadowSize: 0 // Drawing is faster without shadows
-  },
-  yaxis: {
-    min: 0,
-    max: 100,
-    color: "#BBB",
-    tickColor: "#EEE"
-  },
-  xaxis: {
-    show: false
-  },
-  colors: ["#29c75f", "#39c3da"],
-  grid: {
-    margin: 0,
-    borderWidth: 0
-  }
-});
-
-function update() {
-
-  plot.setData([getRandomData(), getRandomData2()]);
-
-  // Since the axes don't change, we don't need to call plot.setupGrid()
-
-  plot.draw();
-  setTimeout(update, updateInterval);
-}
-
-update();
